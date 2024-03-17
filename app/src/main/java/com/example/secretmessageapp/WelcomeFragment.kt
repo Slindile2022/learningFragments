2c9600726e7e1ab18e5a2a4880db22b5c9b456b2
Slindile Maseko <Slindilerowen@gmail.com>
2024-03-17 19:33:08 +0200

    Learning Fragments, Without passing arguments

---
+++ app/src/main/java/com/example/secretmessageapp/WelcomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.secretmessageapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.secretmessageapp.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
